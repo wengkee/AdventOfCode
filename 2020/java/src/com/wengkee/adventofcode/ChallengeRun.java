ee028a90879b79996157d83478b0fb8ed6383fe2
Weng Kee Teh <wengkee.teh@gmail.com>
2021-05-31 01:51:47 +0800

    d8 part 1 and part2

--- 2020/java/src/com/wengkee/adventofcode/ChallengeRun.java
+++ 2020/java/src/com/wengkee/adventofcode/ChallengeRun.java
@@ -1,6 +1,7 @@
 package com.wengkee.adventofcode;
 
 import com.wengkee.adventofcode.day7.BagProcessor;
+import com.wengkee.adventofcode.day8.BootCodeProcessor;
 import com.wengkee.adventofcode.util.ChallengeManager;
 
 import java.io.File;
@@ -12,13 +13,19 @@ public class ChallengeRun {
 
         cm.addChallenge(new BagProcessor(7,1, getResourceFile("d7.txt")));
         cm.addChallenge(new BagProcessor(7,2, getResourceFile("d7.txt")));
-        cm.runChallenge(7,1);
+        cm.addChallenge(new BootCodeProcessor(8,1, getResourceFile("d8.txt")));
+        cm.addChallenge(new BootCodeProcessor(8,2, getResourceFile("d8aa.txt")));
+        cm.runChallenge(8,2);
     }
 
     private static File getResourceFile(String filename){
 
         if ( filename != null ){
-            return new File (ChallengeRun.class.getClassLoader().getResource(filename).getFile());
+            try {
+                return new File (ChallengeRun.class.getClassLoader().getResource(filename).getFile());
+            } catch (Exception e){
+                e.printStackTrace();
+            }
         }
 
         return null;
